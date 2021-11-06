0f00a23d27cca7914a3b3ad42b43194310bbc15f
idadrusdiana <idadrusdiana01@gmail.com>
2021-11-07 00:02:00 +0700

    first commit

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<CENTER>");
+document.writeln("<br>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("<H2>Data Mahasiswa</H2>");
+document.writeln("<H2>Universitas Komputer Indonesia</H2>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("</CENTER");
---
+++ laporan_mahasiswa.js
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Latihan Dengan Objek Select</title>
+    </head>
+    <body>
+        <script language="javascript">
+            kode Javascript disini
+        </script>
+        kode HTML disni
+    </body>
+</html>
